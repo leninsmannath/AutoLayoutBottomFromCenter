9b58ac7299fb82f15184b0c74a241eb374cba8f7
Lenin S <lenin@Lenins-MacBook-Air.local>
2018-03-25 06:20:04 +0530

    Inintial commit

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Lenin S on 25/03/18.
+//  Copyright © 2018 Lworld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
